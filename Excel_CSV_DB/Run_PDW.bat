ca87f0aa6bfe671e7bbffcb3db0c87aa0fe1c204
Luiz Antonio Carlin <luiz.carlin@gmail.com>
2022-12-17 17:45:42 -0300

    MS-DOS Batch file
    And  new version that I try to implement multi-threading

---
+++ Excel_CSV_DB/Run_PDW.bat
@@ -0,0 +1,12 @@
+@echo off
+C:\Users\luizc\AppData\Local\Programs\Python\Python310\python.exe C:\Users\luizc\PycharmProjects\guppe\Excel_CSV_DB\XL_importer.v6.py 
+echo ' '
+echo ' '
+echo ' '
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+set mytime=%time%
+echo Current time is %mydate%  %mytime%
+echo ' '
+echo ' '
+
+pause

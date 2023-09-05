c6fb03c23e5428a5519cc1edb2e683e1be892fdf
Luiz Antonio Carlin <luiz.carlin@gmail.com>
2023-09-05 09:16:07 -0300

    Scripts `Power Shell pra loader

--- Excel_CSV_DB/Run_PDW.bat
+++ Excel_CSV_DB/Run_PDW.bat
@@ -2,7 +2,7 @@
 for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
 echo The Start TIME is: %mydate%  %mytime%
-REM C:\Users\luizc\AppData\Local\Programs\Python\Python310\python.exe C:\Users\luizc\PycharmProjects\guppe\Excel_CSV_DB\XL_importer.v6.py 
+
 cd "C:\Users\luizc\PyCharm\guppe\Excel_CSV_DB\"
 C:\Users\luizc\AppData\Local\Programs\Python\Python311\python.exe C:\Users\luizc\PyCharm\guppe\Excel_CSV_DB\PersonalDataWareHouse.py
 echo ' '

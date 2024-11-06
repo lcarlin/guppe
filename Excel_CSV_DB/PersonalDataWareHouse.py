bbaac6e9bb823758ff2021e122f6a8a724bb3039
Luiz Antonio Carlin <luiz.carlin@gmail.com>
2024-11-06 15:58:58 -0300

    LIttle BUGs

--- Excel_CSV_DB/PersonalDataWareHouse.py
+++ Excel_CSV_DB/PersonalDataWareHouse.py
@@ -698,6 +698,7 @@ def create_pivot_history(data_base_file, types_table, entries_table, out_table_G
     pivot_anual = df_summary.pivot_table(index='Ano', columns='TIPO', values='Debito', aggfunc='sum').fillna(0)
     pivot_anual = pivot_anual[df_types['TIPO']]
     pivot_anual = pivot_anual.reset_index()
+    pivot_anual.to_sql(out_table_Anual , connection, index=False, if_exists="replace")
 
     print('                      ... .. . to Anual total summarized in history ... .. .')
     pivot_anual = df_summary.pivot_table(index='Ano', columns='TIPO', values='Debito', aggfunc='count').fillna(0)

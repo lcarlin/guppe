ff343691d96bab6256628bdb63929bc08e7c9011
Luiz Antonio Carlin <luiz.carlin@gmail.com>
2025-07-15 10:25:10 -0300

    Resumos_In_out Anual

--- Excel_CSV_DB/PersonalDataWareHouse.py
+++ Excel_CSV_DB/PersonalDataWareHouse.py
@@ -295,19 +295,7 @@ def main(param_file):
                                   origem_dados)
 
     end = time.time()
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    total_running_time: str = f"{end - start:8.2f}"
-=======
     total_running_time: str = f"{end - start:7.2f}"
->>>>>>> Stashed changes
-=======
-    total_running_time: str = f"{end - start:7.2f}"
->>>>>>> Stashed changes
-=======
-    total_running_time: str = f"{end - start:7.2f}"
->>>>>>> Stashed changes
     log_line = started + ' Started |' + datetime.datetime.now().strftime(
         "%Y/%m/%d %H:%M:%S") + f' Ended | {total_running_time} TotalSecs | Version {current_version} | Hostname {hostname} | OS {os_pataform}' + '\n'
     log_file.write(log_line)
